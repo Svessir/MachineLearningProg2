d58a349b81a8dbbde06d33a33b5e8567ec0f0078
sverrir <sverrir1993@gmail.com>
2016-10-12 11:27:07 +0000

    initial code

---
+++ src/main/java/is/ru/machineLearning/HelloWorld.java
@@ -0,0 +1,10 @@
+package is.ru.machineLearning;
+
+/**
+ * Created by Sverrir on 12.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
